a909d0862080620e10951730f45880da7a34e124
kobting <kobting0@gmail.com>
2020-10-20 21:47:28 -0400

    Convert from maven to gradle

---
+++ src/main/java/kobting/friendlyminions/helpers/ActionHelper.java
@@ -0,0 +1,15 @@
+package kobting.friendlyminions.helpers;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ActionHelper {
+
+    public static void FixActionValues(AbstractGameAction action){
+
+        ArrayList<String> a = new ArrayList<>();
+    }
+
+}
